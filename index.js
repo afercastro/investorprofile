3295b2fae2acc9c0f150468d3656b431ca2c6a5a
Andres Fernandez <afcastro.ssipp@gmail.com>
2020-06-08 16:55:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ if(time_risk=<7){
     document.write("<h3>You're a moderate investor: </h3><br> <p>For long-term investors who don’t need current income and want some growth potential. Likely to entail some fluctuations in value, but presents less volatility than the overall equity market.</p>"); 
 }else if(time_risk=<40){
     document.write("<h3>You're a moderately aggresive investor: </h3><br> <p>For long-term investors who want good growth potential and don’t need current income. Entails a fair amount of volatility, but not as much as a portfolio invested exclusively in equities.</p>");
-}else if(time_risk>41){
+}else if(time_risk<70){
         document.write("<h3>You're an aggresive investor: </h3><br> <p>For long-term investors who want high growth potential and don’t need current income. May entail substantial year-to-year volatility in value in exchange for potentially high long-term returns.</p>");    
 }
 
