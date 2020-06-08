1714f759370bdc87fd0fdcf9067d840266befdec
Andres Fernandez <afcastro.ssipp@gmail.com>
2020-06-08 20:24:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ if(time_risk<7){
 }else if(time_risk<23){
     document.write(time_risk);
     document.write("<h3>You're a moderately conservative investor: </h3><br> <p>For investors who seek current income and stability, with modest potential for increase in the value of their investments.</p>" );
-}else if(time_risk<30){
+}else if(time_risk<37){
     document.write(time_risk);
     document.write("<h3>You're a moderate investor: </h3><br> <p>For long-term investors who don’t need current income and want some growth potential. Likely to entail some fluctuations in value, but presents less volatility than the overall equity market.</p>"); 
 }else if(time_risk<40){
